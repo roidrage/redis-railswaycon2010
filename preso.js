63c9ba5f0ea0debafce778a8f411022f1213e015
Mathias Meyer <meyer@paperplanes.de>
2010-05-27 17:20:28 +0200

    Make that Scalarium logo a link.

--- preso.js
+++ preso.js
@@ -2,7 +2,10 @@ $(function() {
   setTimeout(function() {
   $('div.scalarium').each(function() {
     console.log(this);
-    $('<img/>').attr("src", "/file/scalarium_logo_small.png").css("position", "absolute").css("top", "-20px").css("left", "100px").css("z-index", "-10").insertBefore($(this));
+    var link = $("<a>").attr("href", "http://www.scalarium.com").attr("target", "_blank");
+    var logo = $('<img/>').attr("src", "/file/scalarium_logo_small.png").css("position", "absolute").css("top", "-20px").css("left", "100px").css("z-index", "-10");
+    link.append(logo);
+    link.insertBefore($(this));
   });
   }, 1000);
 });
